ba03594b80157b66c678477ffe05ab1553e6bcf5
Fasad <salatiksama@gmail.com>
2024-08-02 16:50:26 +0300

    yes

--- src/components/wlcpage/stylesy.js
+++ src/components/wlcpage/stylesy.js
@@ -104,7 +104,7 @@ function Stylesy() {
 
       setIsUserAuthorized(true);
       setId(response.data.id);
-      navigate('/stylesy');
+      navigate('/');
     } catch (error) {
       console.error('Error saving user data:', error.response ? error.response.data : error.message);
     }
