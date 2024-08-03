c7a566b694af957e725f39aeb2c366f3be76a95f
Fasad <salatiksama@gmail.com>
2024-08-03 04:41:52 +0300

    нуы

--- src/components/wlcpage/stylesy.js
+++ src/components/wlcpage/stylesy.js
@@ -53,13 +53,11 @@ function Stylesy() {
   };
 
   const handleSave = async () => {
-    console.log('Starting handleSave...');
-  
     if (!user || !user.id) {
       console.error('Telegram ID is not available');
       return;
     }
-  
+
     const userData = {
       username: nickname || user.username || 'default_username',
       telegram_id: user.id,
@@ -69,48 +67,40 @@ function Stylesy() {
       remaining_invites: 10,
       related_avatar: selectedAvatarId || 1,
     };
-  
-    console.log('User data to be saved:', userData);
-  
+
     try {
-      // Check if the user already exists in the database
       const response = await axios.get(`https://app.jettonwallet.com/api/v1/users/users/?telegram_id=${user.id}`);
       const existingUser = response.data.results[0];
-  
+
       if (existingUser) {
-        // User already exists, update the existing user
         await axios.patch(`https://app.jettonwallet.com/api/v1/users/users/${existingUser.id}/`, { related_avatar: userData.related_avatar });
-  
-        // Update the storedData with the new avatar ID
+
         const storedData = {
-          userId: user.id,
+          userId: existingUser.id,
           telegramId: user.id,
           avatarId: userData.related_avatar,
         };
         localStorage.setItem('userData', JSON.stringify(storedData));
-        
-        console.log('User data updated:', storedData);
       } else {
-        // New user registration
         const createResponse = await axios.post('https://app.jettonwallet.com/api/v1/users/users/', userData);
         
         const newStoredData = {
-          userId: createResponse.data.id, // New ID from the server
+          userId: existingUser.id,
           telegramId: user.id,
           avatarId: createResponse.data.related_avatar,
         };
         localStorage.setItem('userData', JSON.stringify(newStoredData));
         
         setIsUserAuthorized(true);
-        setId(createResponse.data.id); // Set new ID in state
+        setId(createResponse.data.id);
       }
-  
+
       navigate('/');
     } catch (error) {
       console.error('Error saving user data:', error.response ? error.response.data : error.message);
     }
   };
-  
+
   return (
     <div className='wrapper'>
       <div className='headerstyle'>
