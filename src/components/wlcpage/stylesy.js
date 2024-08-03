d4136df07179dc67fe8cfa9c45a6b32d729c1dca
Fasad <salatiksama@gmail.com>
2024-08-03 04:59:46 +0300

    yes

--- src/components/wlcpage/stylesy.js
+++ src/components/wlcpage/stylesy.js
@@ -83,7 +83,7 @@ function Stylesy() {
   
         // Update the storedData with the new avatar ID
         const storedData = {
-          userId: user.id,
+          userId: existingUser.id,
           telegramId: user.id,
           avatarId: userData.related_avatar,
         };
